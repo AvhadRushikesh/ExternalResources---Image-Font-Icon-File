8ce7ab4312f4de2a02446265763ed3b83175dfa3
rushi-smb <rushi@smbios.co.in>
2023-02-24 15:07:37 +0530

    Add project files.

---
+++ ExternalResources/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ExternalResources;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ExternalResources/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ExternalResources;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
